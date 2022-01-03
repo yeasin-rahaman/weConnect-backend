77b7be12a03508f1889cc1d420ff96e100588f67
Dev70 <Shanto9585>
2022-01-03 16:55:04 +0600

    connection

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 var cors = require('cors')
 require('dotenv').config()
 
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
-
+// something 
 const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uc5dq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,14 +17,14 @@ async function run() {
     await client.connect();
     const database = client.db("weConnect");
     const serviceCollection = database.collection("services");
-   
-    app.get("/services",async(req,res)=>{
+
+    app.get("/services", async (req, res) => {
       const query = {}
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.json(services);
     })
-  
+
   } finally {
     // await client.close();
   }
